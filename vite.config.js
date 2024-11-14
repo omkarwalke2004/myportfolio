b41eb140c83372df774136e4d335c5efee07a2d1
omkar walke <omkarwalke647@gmail.com>
2024-11-14 11:51:57 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/myportfolio/', // Update with your actual GitHub repository name
   plugins: [react()],
   build: {
     rollupOptions: {
